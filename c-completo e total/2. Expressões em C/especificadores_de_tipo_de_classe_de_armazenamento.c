92e8d7e73f8f217950089c795df3002217bf8ea8
Mariana Paris <155462134+mariana2908@users.noreply.github.com>
2025-03-08 17:33:03 +0000

    primeira parte de variáveis register

--- c-completo e total/2. Expressões em C/especificadores_de_tipo_de_classe_de_armazenamento.c
+++ c-completo e total/2. Expressões em C/especificadores_de_tipo_de_classe_de_armazenamento.c
@@ -133,4 +133,96 @@ riável global e, em seguida, ajustar seu valor de acordo com o especificado.
 Porém, *series_num* foi feita *static* justamente para não ser definida como 
 global. Isso leva ao segundo uso de *static*.*/
 
-// Variáveis Globais static
+// Variáveis Globais static (pag. 46)
+/* Alicar o especificador *static* a uma variável global informa ao compila-
+dor para criar uma variável global que é reconhecida apenas no arquivo no 
+qual a mesma foi declarada. Isso significa que, muito embora a variável seja 
+global, rotinas em outros arquivos não podem reconhecê-la ou alterar seu con-
+teúdo diretamente; assim, não está sujeita a efeitos colaterais. Entretanto, 
+para as poucas situações onde uma variável local *static* não possa fazer o 
+trabalho, você criar um pequeno arquivo que contenha apenas as funções que 
+precisam da variável global *static* e compilaar separadamente esse arquivo 
+sem medo de efeitos colaterais.
+   Para ilustrar uma variável global *static*, o exemplo de gerador de série
+da seçaõ anterior foi recodificado de forma que um valor somente inicialize 
+a série por meio de uma chamada a uma segunda função denominada *series_-
+-start()*. O arquivo inteiro, que contém *series()*, *series_start()* e 
+*series_num* é mostrado aqui:*/
+// Isso deve estar em um único arquivo - preferencialmente isolado.
+static int series_num;
+void series_start(int seed);
+int series(void);
+series (void) {
+    series_num = series_num+23;
+    return series_num;
+}
+
+// Inicializa series_num
+void series_start(int seed) {
+    series_num = seed;
+}
+
+/* Para inicializar o gerador de série, deve-se chamar *series_start()* 
+com algum valor inteiro conhecido. Depois disso, chamadas a *series()* 
+geram os pr´´oximos elementos da série.
+   _Revisando_: Os nomes das variáveis locais *static* são reconhecidos apenas
+na função ou bloco de código em que elas são declaradas. Os nomes das variá-
+veisglobais *static* são reconhecidos apenas no arquivo em que elas residem. 
+Isso significa que, se vocêcolocar as funções *series()* e *series_start()* em
+uma biblioteca, poderá usar as funções, mas não poderá referenciar a variável
+*series_num*, que está escodida do resto do código de seu programa. De fato, 
+você pode, inclusive, declarar e usar outra variável chamada *series_num* em 
+seuprograma (em outro arquivo, é claro). Em essência, o modificador *static*
+permite variáveis que são reconhecidaas pelas funções que precisam delas, sem 
+confundir outras funções.
+   As variáveis *static* admitem que você, o programador, esconda porções de 
+seu programa das outras partes. Isso pode ser uma vantagem imensa quando se 
+tenta gerenciar um programa muito grande e complexo. O especificador de clas-
+se de armazenamento *static* deixa você criar funções gerais que podem ir pa-
+ra bibliotecas que serão utilizadas posteriormente.*/
+
+// Variáveis register (pág. 47)
+/* O especificador de armazenamento *register* tradicionalmente era aplicado 
+apenas a variáveis dod tipos *int* e *char*. Contudo, o padrão C ANSI ampliou
+sua definição de forma que ele possa ser aplicado a qualquer variável.
+   Originalmente, o especificador *register* solicitava ao compilador C que 
+armazenasse o valor das variáveis declaradas com esse especificador num regis-
+trador da CPU em vez da memória, onde as variáveis normais são armazenadas. Is-
+so significa que operações com variáveis *register* poderiam ocorrer muito mais 
+rapidamente que nas variáveis armazenadas na memória, pois o valor dessas va-
+riáveis era realmente conservado na CPU e não era necessário acesso à memória 
+para determinar ou modificar seus valores.
+   Hoje, uma vez que agora o padrão C ANSI permite que você modifique qualquer 
+tipo de variável com *register*, ele alterou a definição do que *register* faz.
+O padrão C ANSI simplismente determina que "o acesso ao objeto é o mais rápido
+possível". Na prática, caracteres e inteiros são colocados nos registradores
+da CPU. Objetos maiores, como matrizes, obviamente não podem ser armazenados 
+em um registrador, mas eles ainda podem receber um tratamento diferenciado.
+Dependendo da implementação do compilador C e de seu ambiente operacional, va-
+riáveis *register* podem ser manipuladas de quaisquer formas consideradas cabí-
+veis pelo implementador do compilador. O padrão C ANSI também permite que o 
+compilador ignore o especificador *register* e trate as variáveis modificadas 
+por ele como se não fossem, mas isso raramente ocorre na prática.
+   Você só pode aplicar o especificador *register* a variáveis locais e a parâ-
+metros formais em uma função. Assim, variáveis globais *register* naõ são permi-
+mitidas. Aqui está um exemplo de como declarar uma variável *register* do tipo
+*int* e usá-la para controlar um laço. Essa função calcula o resultado de M^e 
+para inteiros:*/
+
+int_pwr(register int m, register int e) {
+    register int temp;
+    
+    temp = 1;
+
+    for(; e; e--) temp = temp * m;
+    return temp;
+}
+
+/* Nesse exemplo, tanto *e* quanto *m* e *temp* são declaradas como variáveis
+*register* porque são usadas dentro do laço. O fato de variáveis *register* se-
+rem otimizadas para velocidade torna-as ideais ao controle de laço. Geralmente, 
+variáveis *register* são usadas quando mais apropriadas, isto é, em lugares on-
+de são feitas muitas referências a uma variável. Isso é importante porque você 
+pode declarar qualquer número de variáveis como sendo do tipo *register*, mas 
+nem todas recebem a mesma otimização de velocidade.
+*/ 
